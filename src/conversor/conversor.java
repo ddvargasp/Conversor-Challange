b10c1ddefcaffd324c0da78fe3a4b1c9496d0f0d
ddvargasp <109007237+ddvargasp@users.noreply.github.com>
2023-05-25 20:13:42 -0600

    first commit

---
+++ src/conversor/conversor.java
@@ -0,0 +1,15 @@
+package conversor;
+
+import conversor.igu.Principal;
+
+public class conversor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Principal princ = new Principal();
+		princ.setVisible(true);
+		princ.setLocationRelativeTo(null);
+
+	}
+
+}
